496373e38ac2219396fbac8e115f47e4a53adfa5
Memo G <74260113+Gryphus97@users.noreply.github.com>
2022-06-15 17:14:15 -0600

    Archivos de proyecto
    Se agregan archivos necesarios para integridad y funncionalidad

---
+++ Interfaz_grafica/Conexion.vb
@@ -0,0 +1,15 @@
+﻿
+'Módulo para definición comandos a enviar por consultas a SQL
+Module Conexion
+
+    '********************************************************************************************************************************************************************************************
+    'Conexión con base de datos. Modificar según el servidor donde esté alojada
+    Public queryString As String = "Server=UVECASE-Champion;Database=instrucciones"
+    Public Conn As New MySql.Data.MySqlClient.MySqlConnection() 'Declaración de objeto de conexión
+    Public cmd As New MySql.Data.MySqlClient.MySqlCommand(proc, Conn) 'Declaración de objeto de comando
+
+    '********************************************************************************************************************************************************************************************
+    'Definición de comandos para interactuar con base de datos.
+    Public proc As String = " "
+
+End Module
