84672a10fb9ed9a87a6de606a8d695d30db5d0eb
Memo G <74260113+Gryphus97@users.noreply.github.com>
2022-06-15 16:57:41 -0600

    Delete Interfaz_Gráfica - DB-UVECASE directory

--- Interfaz_Gráfica - DB-UVECASE/Lista_Equipos.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Lista_Equipos
-    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
-        Main_app.Show()
-        Me.Hide()
-    End Sub
-End Class
--- Interfaz_Gráfica - DB-UVECASE/UVECASE-DB/Giras/Giras_liquidaciones.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Giras_liquidaciones
-    Private Sub Exitform(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
-        Main_app.Show()
-    End Sub
-
-End Class
